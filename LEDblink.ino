5edc5eef27f7a5edd4c8d6f4dcf0fdecc388915d
azizmaulana <vvvvvv.ateng@gmail.com>
2016-08-29 19:26:42 +0700

    Add files via upload

---
+++ LEDblink.ino
@@ -0,0 +1,14 @@
+int led = 13;
+void setup() {
+pinMode(led, OUTPUT);  // put your setup code here, to run once:
+
+}
+
+void loop() {
+digitalWrite(led, 1);
+delay (3000);
+digitalWrite(led, 0);
+delay (1000);
+// put your main code here, to run repeatedly:
+
+}
